5b942c04c3b7ad1e6fccd41be9e9e1b793465188
thushara-abeykoon <thushara.abeykoon99@gmail.com>
2024-09-29 09:11:22 +0530

    created dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Login from "./screens/login/Login";
 import lightTheme from "./assets/theme/light";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Dashboard from "./screens/dashboard/Dashboard";
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <div>
-        <Login />
+        <Dashboard />
       </div>
     </ThemeProvider>
   );
---
+++ src/screens/dashboard/Dashboard.tsx
@@ -0,0 +1,65 @@
+import { Box } from "@mui/material";
+import TataCommunicationFlatImage from "../../assets/images/tata-communications-flat.png";
+import CustomTypography, {
+  fontWeights,
+} from "../../components/CustomTypography";
+import ElementBoxImage from "../../assets/images/elementbox-image.jpeg";
+
+const Dashboard = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "rgba(255,255,255,0.9)",
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+        gap:"110px"
+      }}
+    >
+      <img src={TataCommunicationFlatImage} width="862px" />
+      <Box sx={{display:"flex", alignItems:"center", justifyContent:"center", gap:"72px"}}>
+        <ElementBox name="Customer Management" onClick={() => {}} />
+        <ElementBox name="Inventory Management" onClick={() => {}} />
+      </Box>
+    </div>
+  );
+};
+
+const ElementBox: React.FC<{ name: string; onClick: () => void }> = ({
+  name,
+  onClick,
+}) => {
+  return (
+    <Box
+      onClick={onClick}
+      sx={{
+        width: "456px",
+        height: "300px",
+        borderRadius: "20px",
+        padding: "32px",
+        backgroundImage: `linear-gradient(rgba(0, 48, 131, 0.8), rgba(0, 48, 131, 0.8)), url(${ElementBoxImage})`,
+        textAlign: "center",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor:"pointer",
+        ":hover": {
+          backgroundImage: `linear-gradient(rgba(0, 32, 96, 0.8), rgba(0, 32, 96, 0.8)), url(${ElementBoxImage})`,
+        },
+      }}
+    >
+      <CustomTypography
+        fontWeight={fontWeights.lg}
+        fontSize={"48px"}
+        sx={{ color: "white" }}
+      >
+        {name}
+      </CustomTypography>
+    </Box>
+  );
+};
+
+export default Dashboard;
