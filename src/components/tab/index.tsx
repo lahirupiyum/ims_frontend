3799c960b88f54b0897d3749eb88d3ba88c820eb
Lahiru Piyamantha <lahirupiyumanthahemathilake@gmail.com>
2024-10-05 18:58:11 +0530

    Created TabContainer custom component

---
+++ src/components/tab/index.tsx
@@ -0,0 +1,51 @@
+import { Box } from "@mui/material";
+import ContainedButton from "../buttons/ContainedButton";
+import { fontSizes } from "../typography/CustomTypography";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export interface TabElement {
+  label: string;
+  contextPath: string;
+}
+
+const TabContainer: React.FC<{ elements: TabElement[] }> = ({ elements }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: 0,
+        justifyContent: "start",
+        borderRadius: "20px",
+      }}
+    >
+     {elements.map(element => <TabButton element={element} />)}
+    </Box>
+  );
+};
+
+const TabButton: React.FC<{ element: TabElement }> = ({ element }) => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const isSelected = pathname === element.contextPath;
+
+  return (
+    <ContainedButton
+      sx={{
+        bgcolor: isSelected? "black" : "white",
+        color: isSelected? "white" : "black",
+        borderRadius:"0",
+        fontSize: fontSizes.xs,
+        ":hover": {
+          bgcolor: isSelected? "" :"lightblue",
+        },
+        transition:"all 0.2s"
+      }}
+      onClick={()=> navigate(element.contextPath)}
+    >
+      {element.label}
+    </ContainedButton>
+  );
+};
+
+export default TabContainer;
