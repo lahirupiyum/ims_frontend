371a834f580b335f81dfb1468c26c7d655b54ef4
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-11-24 14:09:29 +0530

    updated not_found section

--- src/screens/notfound/index.tsx
+++ src/screens/notfound/index.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@mui/material";
+import NotFoundGIF from "../../assets/images/not_found.gif";
 
 const NotFound = () => {
   return (
@@ -15,16 +15,8 @@ const NotFound = () => {
         alignItems: "center",
       }}
     >
-      <Card sx={{ width: "400px", p: "30px", textAlign: "center" }}>
-        <img
-          style={{ width: "100%", borderRadius: "400px" }}
-          src="https://steamuserimages-a.akamaihd.net/ugc/170412655659465026/9D6C188AFF8F415A929DDCE546ACF1A8F0D58256/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
-        />
-        <p style={{ fontSize: "30px", fontWeight: "600" }}>
-          YOU PICK THE WRONG HOUSE FOOL!!!
-        </p>
-        <p>404 NOT FOUND</p>
-      </Card>
+      
+      <img src={NotFoundGIF} />
     </div>
   );
 };
