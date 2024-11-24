b64b500536ae7fe22fa0dc8a5ec368b772622e3c
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-11-24 09:29:50 +0530

    applied delete dialog for network device type

--- src/screens/management/inventory/networkassets/types.tsx
+++ src/screens/management/inventory/networkassets/types.tsx
@@ -5,10 +5,12 @@ import CustomTable, {
   Column,
   wrapActionButtons,
 } from "../../../../components/table";
-import { useAppSelector } from "../../../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../../../redux/hooks";
 import { typePageAction } from "../../../../redux/slices/network/type/page";
 import { NetworkDeviceTypeResponse } from "../../../../types/NetworkDeviceType";
 import NetowrkDeviceTypeForm from "./create-update-form/type";
+import { networkDeviceTypeDeleteAction } from "../../../../redux/slices/network/type/delete";
+import DeleteDialog from "../../../../components/delete-dialog";
 
 const columns: Column[] = [
   { id: "action", label: "Action", minWidth: 50 },
@@ -21,6 +23,11 @@ const Type = () => {
   const [selectedType, setSelectedType] =
     useState<NetworkDeviceTypeResponse | null>(null);
   const [selectedIndex, setSelectedIndex] = useState<number>(-1);
+
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState<boolean>(false);
+  const [selectedIdToDelete, setSelectedIdToDelete] = useState<number>(-1);
+
+  const dispatch = useAppDispatch();
 
   const deviceTypePageState = useAppSelector(
     (state) => state.networkDeviceType.page
@@ -38,6 +45,21 @@ const Type = () => {
     setSelectedIndex(-1);
   };
 
+  const handleOpenDeleteDialog = (id: number) => {
+    setIsDeleteDialogOpen(true);
+    setSelectedIdToDelete(id);
+  };
+
+  const hanldeCloseDeleteDialog = () => {
+    setIsDeleteDialogOpen(false);
+    setSelectedIdToDelete(-1);
+  };
+
+  const deleteFunction = () => {
+    dispatch(networkDeviceTypeDeleteAction(selectedIdToDelete));
+  };
+
+
   const rowsFormatter = (rows: NetworkDeviceTypeResponse[]) =>
     rows.map(({ id, name }, index) => ({
       action: wrapActionButtons([
@@ -48,7 +70,7 @@ const Type = () => {
           },
           1
         ),
-        actionButton(ActionIcontype.delete, () => {}, 2),
+        actionButton(ActionIcontype.delete, () => handleOpenDeleteDialog(id), 2),
       ]),
       id,
       name,
@@ -67,6 +89,12 @@ const Type = () => {
         handleClose={handleCloseUpdateForm}
         index={selectedIndex}
         selectedType={selectedType}
+      />
+       <DeleteDialog
+        deleteFunction={deleteFunction}
+        handleClose={hanldeCloseDeleteDialog}
+        name="Network Device Type"
+        open={isDeleteDialogOpen}
       />
     </>
   );
