87d4e95874c26c4217c3a71a727b44bc7628d6d2
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-23 00:07:52 +0530

    Update routesConfig.tsx

--- src/route/config/routesConfig.tsx
+++ src/route/config/routesConfig.tsx
@@ -5,33 +5,33 @@ import Login from "../../screens/login/Login";
 import Inventory from "../../screens/management/inventory";
 import NotFound from "../../screens/notfound";
 import { home, inventory, login } from "../../utils/context-paths";
-import { getInventoryRoutes } from "./inventory/inventoryRoutes";
+import { inventoryRoutes } from "./inventoryRoutes";
 
-export const getPathElement: (
+interface RouteObject{
   path: string,
   element: ReactNode,
   children: RouteObject[]
-) => RouteObject = (path, element, children) => ({ path, element, children });
+};
 
 const routesConfig: RouteObject[] = [
   {
     path:home, 
-    element: <Home />, 
+    element: <Home /> as React.ReactNode, 
     children: []
   },
   {
     path: login,
-    element: <Login />, 
+    element: <Login /> as React.ReactNode, 
     children: []
   },
   {
     path: inventory,
-    element: <Inventory />,
-    children: Object.values(getInventoryRoutes())
+    element: <Inventory /> as React.ReactNode,
+    children: inventoryRoutes
   },
   {
     path: "*", 
-    element: <NotFound />, 
+    element: <NotFound /> as React.ReactNode, 
     children: []
   }
 ];
