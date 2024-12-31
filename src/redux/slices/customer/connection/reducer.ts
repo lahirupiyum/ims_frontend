83225d07f3737e7dedb8f37e6682eb96d910ffe7
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-12-31 09:59:14 +0530

    added page reducers for mpls and ill

--- src/redux/slices/customer/connection/reducer.ts
+++ src/redux/slices/customer/connection/reducer.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import page from "./page";
+import ill from "./ill/reducer";
+import mpls from "./mpls/reducer";
 
 const connection = combineReducers({
-    page
+    page,
+    ill,
+    mpls
 })
 
 export default connection;
