22887654f9447623f76311d93152212be6907306
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-31 19:14:37 +0530

    Update index.tsx

--- src/screens/management/inventory/networkassets/index.tsx
+++ src/screens/management/inventory/networkassets/index.tsx
@@ -4,13 +4,13 @@ import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import DropdownButton, {
   MenuElement,
 } from "../../../../components/buttons/DropdownButton";
-import TabContainer, { TabElement } from "../../../../components/tab";
-import {
-  inventory_network_devices,
-  inventory_network_manufacturers,
-  inventory_network_models,
-  inventory_network_types,
-} from "../../../../utils/context-paths";
+// import TabContainer, { TabElement } from "../../../../components/tab";
+// import {
+//   inventory_network_devices,
+//   inventory_network_manufacturers,
+//   inventory_network_models,
+//   inventory_network_types,
+// } from "../../../../utils/context-paths";
 import FormHandler, { FormTypes } from "./create-update-form/FormHandler";
 
 const NetworkAssets = () => {
@@ -24,24 +24,24 @@ const NetworkAssets = () => {
     if (pathname === "/inventory/asset/network") navigate("devices");
   });
 
-  const tabs: TabElement[] = [
-    {
-      label: "Network Devices",
-      contextPath: inventory_network_devices,
-    },
-    {
-      label: "Manufacturers",
-      contextPath: inventory_network_manufacturers,
-    },
-    {
-      label: "Device Types",
-      contextPath: inventory_network_types,
-    },
-    {
-      label: "Device Models",
-      contextPath: inventory_network_models,
-    },
-  ];
+  // const tabs: TabElement[] = [
+  //   {
+  //     label: "Network Devices",
+  //     contextPath: inventory_network_devices,
+  //   },
+  //   {
+  //     label: "Manufacturers",
+  //     contextPath: inventory_network_manufacturers,
+  //   },
+  //   {
+  //     label: "Device Types",
+  //     contextPath: inventory_network_types,
+  //   },
+  //   {
+  //     label: "Device Models",
+  //     contextPath: inventory_network_models,
+  //   },
+  // ];
   
   const handleCloseDropDown = () => {
     setOpenDropDown(false);
@@ -71,7 +71,7 @@ const NetworkAssets = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <TabContainer elements={tabs} />
+        {/* <TabContainer elements={tabs} /> */}
         <DropdownButton name="new" elements={menuElements} isOpen={openDropDown} setIsOpen={setOpenDropDown} />
       </Box>
       <Outlet />
