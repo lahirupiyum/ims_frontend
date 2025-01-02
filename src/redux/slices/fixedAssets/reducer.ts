3002d4c53f6015de55c3f70aa5bfdee78b16e76f
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-01-02 23:44:55 +0530

    Create reducer.ts

---
+++ src/redux/slices/fixedAssets/reducer.ts
@@ -0,0 +1,16 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import page from "./page";
+// import create from "./create";
+// import update from "./update";
+import deleteState from "./delete";
+import list from "./list";
+
+const fixedAssets = combineReducers({
+    page,
+    // create,
+    // update,
+    delete: deleteState,
+    list,
+})
+
+export default fixedAssets;
