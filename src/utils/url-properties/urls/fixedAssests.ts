940dbc4498ad5d92a77006bb862fd4537d78ca2a
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-01-02 23:44:20 +0530

    Create fixedAssests.ts

---
+++ src/utils/url-properties/urls/fixedAssests.ts
@@ -0,0 +1,5 @@
+import { getListUrl, getUrl, mergeWithId } from "../urlGenerator";
+
+export const fixedAssetUrl = getUrl("asset/fixedassest/");
+export const fixedAssetListUrl = getListUrl(fixedAssetUrl);
+export const fixedAssetWithIdUrl = (id: number) => mergeWithId(fixedAssetUrl, id);
