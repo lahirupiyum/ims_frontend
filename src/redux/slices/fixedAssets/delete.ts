7b85d489c74891afa0850101481dadf814881305
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-01-02 23:45:03 +0530

    Create delete.ts

---
+++ src/redux/slices/fixedAssets/delete.ts
@@ -0,0 +1,26 @@
+import { FixedAssetResponse } from "../../../types/Inventory/asset/FixedAssets";
+import { fixedAssetWithIdUrl } from "../../../utils/url-properties/urls/fixedAssests";
+import globalDeleteAction from "../../actions/globalDeleteAction";
+import DeleteSliceActionType from "../../types/DeleteActionType";
+import getDeleteSlice from "../config/globalDeleteSlice";
+import { fixedAssetPageAction } from "./page";
+
+const fixedAssetDeleteSlice = getDeleteSlice<FixedAssetResponse>("fixedAssetDelete");
+
+const { request, success, reject } = fixedAssetDeleteSlice.actions;
+
+const deleteActionTypes: DeleteSliceActionType<FixedAssetResponse> = {
+  request,
+  success,
+  reject,
+};
+
+export const fixedAssetDeleteAction = (id: number) =>
+  globalDeleteAction<FixedAssetResponse>(
+    fixedAssetWithIdUrl(id),
+    deleteActionTypes,
+    fixedAssetPageAction
+  );
+
+export const { reset: fixedAssetDeleteReset } = fixedAssetDeleteSlice.actions;
+export default fixedAssetDeleteSlice.reducer;
